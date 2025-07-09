c119786bc448a3e898d60dda6a67aae626c0e84b
G <giovanni@hertz143.local>
2025-07-09 12:31:29 +0300

    Обновил стили и карточки

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,16 @@
-// src/firebase.js
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// Если нужно хранить файлы (картинки) в Firebase Storage:
-import { getStorage } from "firebase/storage";
-import { db } from "./firebase";
-
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8an2JyS6E4euGDi8v85Hrr3g2Dyexhk",
-  authDomain: "friendstore-ebc1d.firebaseapp.com",
-  projectId: "friendstore-ebc1d",
-  storageBucket: "friendstore-ebc1d.appspot.com",
-  messagingSenderId: "263889239019",
-  appId: "1:263889239019:web:023da8fbb6dc34f8ba8a5e",
+  apiKey: "AIzaSyBBan2jySS6E4euGDi0v85Hrr3gZDyexhk",
+  authDomain: "friendsstore-ebc1d.firebaseapp.com",
+  projectId: "friendsstore-ebc1d",
+  storageBucket: "friendsstore-ebc1d.firebasestorage.app",
+  messagingSenderId: "268389293019",
+  appId: "1:268389293019:web:023da8fbb6dc34f8b8a5fe",
   measurementId: "G-31774XTPJS"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);       // Это твоя база (Firestore)
-export const storage = getStorage(app);    // Это хранилище файлов (Storage, если надо)
+export const db = getFirestore(app);
